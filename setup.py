1bc497d302be19cc5800669b51379a695bbda4d1
Vitor <skullventuzelos@hotmail.com>
2022-04-12 14:11:31 +0100

    Renamed package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['ros1_flatland_quick_start'],
+    packages=['flatland_quick_start_ros1'],
     package_dir={'': 'include'},
 )
 
